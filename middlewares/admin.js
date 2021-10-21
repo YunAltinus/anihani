a51c4739dbe94ffe83ca6baf8bdd7398702f9800
Yunus Altın <yunusaltin1963@gmail.com>
2021-10-22 00:07:56 +0300

    Update admin.js

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,10 +1,11 @@
 const adminMiddleware = async (req, res, next) => {
-  if (req.user?.isAdmin) next()
+  if (!req.user?.isAdmin)
+    res.status(403).json({
+      success: false,
+      message: "Bu alan için yetkiye sahip değilsiniz!",
+    })
 
-  res.status(403).json({
-    success: false,
-    message: "Bu alan için yetkiye sahip değilsiniz!",
-  })
+  next()
 }
 
 module.exports = adminMiddleware
