edd6a6218d0dbbde69d5d9bc4798c62067aef067
Yunus Altın <yunusaltin1963@gmail.com>
2021-10-24 02:15:08 +0300

    Update login and Add bcrypt

--- services/user-service.js
+++ services/user-service.js
@@ -1,11 +1,32 @@
 const BaseService = require("./base-service")
 const User = require("../models/User")
-const jwt = require("jsonwebtoken")
 const schema = require("../lib/validation")
+const createError = require("http-errors")
+const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
 
 class UserService extends BaseService {
-  getTokenFromUser(id) {
+  async login(email, password) {
+    const user = await this.findOne(email)
+
+    const checkedPassword = await bcrypt.compare(password, user?.password)
+
+    if (!user || !checkedPassword)
+      throw createError(400, "Girilen email/şifre hatalıdır!")
+
+    return user
+  }
+
+  async preSaveHashToPassword(user) {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(user.password, salt)
+
+    user.password = hash
+
+    return user
+  }
+
+  getTokenFromUserService(id) {
     const { JWT_SECRET_KEY, JWT_EXPIRE } = process.env
 
     const token = jwt.sign({ id }, JWT_SECRET_KEY, {
@@ -15,27 +36,16 @@ class UserService extends BaseService {
     return token
   }
 
-  async joiValidationForRegister(user) {
+  async joiValidation(user) {
     return schema.validateAsync(user)
   }
 
   toJSON(user) {
-    delete user._id
-    delete user.password
-    delete user.createdAt
-    delete user.updatedAt
-    delete user.__v
-
-    return user
-  }
-
-  async login(email, password) {
-    const user = await this.findOne({ email })
-
-    const checkedPassword = await bcrypt.compare(password, user?.password)
-
-    if (!user || !checkedPassword)
-      throw createError(400, "Girilen email/şifre hatalıdır!")
+    delete user["password"]
+    delete user["createdAt"]
+    delete user["updatedAt"]
+    delete user["__v"]
+    console.log("json", user)
 
     return user
   }
