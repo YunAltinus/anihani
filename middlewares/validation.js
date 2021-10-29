1f88a719d9afc08b78a76693c3ada78893d6b4dc
Yunus Altın <yunusaltin1963@gmail.com>
2021-10-29 17:42:17 +0300

    Create validation.js

---
+++ middlewares/validation.js
@@ -0,0 +1,10 @@
+const validate = (schema) => async (req, res, next) => {
+  try {
+    await schema.validateAsync(req.body)
+    next()
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = validate
