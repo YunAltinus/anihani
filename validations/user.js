9c80a743be5a955edad048ff5120a838c50bdba3
Yunus Altın <yunusaltin1963@gmail.com>
2021-10-29 17:42:07 +0300

    Create user.js

---
+++ validations/user.js
@@ -0,0 +1,46 @@
+const Joi = require("joi")
+
+const registerValidation = Joi.object({
+  username: Joi.string()
+    .pattern(new RegExp("^[a-zA-Z0-9]{2,30}$"))
+    .required()
+    .trim(),
+
+  password: Joi.string()
+    .pattern(new RegExp("^[a-zA-Z0-9]{6,30}$"))
+    .required()
+    .trim(),
+
+  repeatPassword: Joi.ref("password"),
+
+  birth: Joi.string().trim(),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+
+  isAdmin: Joi.boolean().default(false),
+}).with("password", "repeatPassword")
+
+const loginValidation = Joi.object({
+  token: Joi.string(),
+
+  username: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{2,30}$")).trim(),
+
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{6,30}$")).trim(),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .trim(),
+})
+
+module.exports = {
+  registerValidation,
+  loginValidation,
+}
