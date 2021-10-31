7ed82bda96ac99ec3c2189fd563dcd117da7f576
Yunus Altın <yunusaltin1963@gmail.com>
2021-10-31 19:52:31 +0300

    Fix bug

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,31 @@
 const createError = require("http-errors")
 const { userService } = require("../services")
 
+const getAllUser = async (req, res, next) => {
+  try {
+    const lastUsers = await userService.load()
+
+    if (!lastUsers) res.status(404).json({ message: "Kullanicilar bulunamadi" })
+
+    const users = [...lastUsers]
+
+    return res.status(200).json({ users })
+  } catch (error) {
+    next(createError(400, "kullanici bulunamadi"))
+  }
+}
+
 const getUserProfile = async (req, res, next) => {
   try {
-    const user = await userService.findById(req.params?.userId)
+    const lastUser = await userService.findById(req.params?.userId)
 
-    if (!user) res.status(404).json({ message: "Kullanici bulunamadi" })
+    if (!lastUser) res.status(404).json({ message: "Kullanici bulunamadi" })
+
+    const user = {
+      ...lastUser.toObject(),
+    }
+
+    delete user["password"]
 
     return res.status(200).json({ user })
   } catch (error) {
@@ -17,23 +37,23 @@ const register = async (req, res, next) => {
   try {
     const hashToUser = await userService.preSaveHashToPassword(req.body)
 
-    const user = await userService.insert(hashToUser)
-    if (!user) res.status(500).json({ message: "Kullanici olusturulamadi" })
+    const lastUser = await userService.insert(hashToUser)
+    if (!lastUser) res.status(500).json({ message: "Kullanici olusturulamadi" })
 
-    const accessToken = userService.generateAccessToken(user)
-    const refleshToken = userService.generateRefleshToken(user)
+    const accessToken = userService.generateAccessToken(lastUser)
+    const refleshToken = userService.generateRefleshToken(lastUser)
 
-    const newUser = {
-      ...user?.toObject(),
+    const user = {
+      ...lastUser?.toObject(),
       token: {
         accessToken,
         refleshToken,
       },
     }
 
-    delete newUser.password
+    delete user.password
 
-    return res.json(newUser)
+    return res.json(user)
   } catch (err) {
     next(createError(400, err))
   }
@@ -43,53 +63,50 @@ const login = async (req, res, next) => {
   const { email, password } = req.body
 
   try {
-    const user = await userService.login(email, password)
+    const lastUser = await userService.login(email, password)
 
-    const accessToken = userService.generateAccessToken(user)
-    const refleshToken = userService.generateRefleshToken(user)
+    const accessToken = userService.generateAccessToken(lastUser)
+    const refleshToken = userService.generateRefleshToken(lastUser)
 
-    const newUser = {
-      ...user.toObject(),
+    const user = {
+      ...lastUser.toObject(),
       token: {
         accessToken,
         refleshToken,
       },
     }
 
-    delete newUser.password
+    delete user.password
 
-    return res.json(newUser)
+    return res.json(user)
   } catch (err) {
     next(err)
   }
 }
 
 const updateMyProfile = async (req, res, next) => {
-  const { id } = req.user
+  const { _id } = req.user
+  const updateUser = req.body
 
   try {
-    await userService.joiValidation(req.body)
+    if (updateUser.password)
+      updateUser = await userService.preSaveHashToPassword(updateUser)
 
-    const hashToUser = await userService.preSaveHashToPassword(req.body)
-
-    const updatedUser = await userService.update(id, hashToUser, {
-      new: true,
-      runValidators: true,
-    })
-
-    if (!updatedUser) next(createError(404, "Kullanıcı güncellenemedi!"))
+    const user = await userService.update(_id, updateUser)
+    if (!user) next(createError(400, "Kullanici guncellenemedi"))
 
     return res.json({
       message: "Kullanıcı güncellendi!",
-      updatedUser,
     })
   } catch (err) {
     next(err)
   }
 }
+
 module.exports = {
   getUserProfile,
   register,
   updateMyProfile,
   login,
+  getAllUser,
 }
