ce1ef5dc8d5f1c9f9a8ee3e44a4d77554e219f6d
Yunus Altın <yunusaltin1963@gmail.com>
2021-10-31 19:54:54 +0300

    Update validation.js

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,6 +1,6 @@
 const validate = (schema) => async (req, res, next) => {
   try {
-    await schema.validateAsync(req.body)
+    const value = await schema.validateAsync(req.body)
     next()
   } catch (err) {
     next(err)
